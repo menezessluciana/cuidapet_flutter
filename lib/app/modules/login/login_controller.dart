c165e0cda6baae2eaade2b8418ecd7526cce01ae
Luciana Menezes <luciana.menezes@superlogica.com>
2021-02-10 07:01:08 -0300

    implementando login no controller

--- lib/app/modules/login/login_controller.dart
+++ lib/app/modules/login/login_controller.dart
@@ -1,3 +1,7 @@
+import 'package:cuidapet_curso/app/core/exceptions/cuidapet_exceptions.dart';
+import 'package:cuidapet_curso/app/services/usuario_service.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:mobx/mobx.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
@@ -7,11 +11,33 @@ part 'login_controller.g.dart';
 class LoginController = _LoginControllerBase with _$LoginController;
 
 abstract class _LoginControllerBase with Store {
+  final UsuarioService _service;
+  GlobalKey<FormState> formKey = GlobalKey();
+  TextEditingController loginController = TextEditingController();
+  TextEditingController senhaController = TextEditingController();
+
   @observable
-  int value = 0;
+  bool obscureText = true;
+
+  _LoginControllerBase(this._service);
 
   @action
-  void increment() {
-    value++;
+  Future<void> login() async {
+    if (formKey.currentState.validate()) {
+      try {
+        await _service.login(loginController.text, senha: senhaController.text);
+        Modular.to.pushReplacementNamed('/');
+      } on AcessoNegadoException catch (e) {
+        print(e);
+        Get.snackbar('Erro', 'Login ou senha inválidos');
+      } catch (e) {
+        Get.snackbar('Erro', 'Erro ao realizar login');
+      }
+    }
+  }
+
+  @action
+  void mostrarSenhaUsuario() {
+    obscureText = !obscureText;
   }
 }
