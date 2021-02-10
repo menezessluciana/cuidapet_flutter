29b9b17e3525f67f3f2df036709288b3bdd3532c
Luciana Menezes <luciana.menezes@superlogica.com>
2021-02-10 07:02:08 -0300

    alterando nome de password para senha no repository

--- lib/app/repository/usuario_repository.dart
+++ lib/app/repository/usuario_repository.dart
@@ -7,24 +7,24 @@ import 'package:cuidapet_curso/app/models/confirm_login_model.dart';
 import 'package:cuidapet_curso/app/repository/shared_prefs_repository.dart';
 
 class UsuarioRepository {
-  
   //Password é opcional pois o usuário pode logar com o facebook
-    Future<AccessTokenModel> login(String email, {String password, facebookLogin = false, String avatar =''}){
+  Future<AccessTokenModel> login(String email,
+      {String senha, facebookLogin = false, String avatar = ''}) {
     return CustomDio.instance.post('/login', data: {
       'login': email,
-      'senha': password,
+      'senha': senha,
       'facebookLogin': facebookLogin,
       'avatar': avatar,
     }).then((res) => AccessTokenModel.fromJson(res.data));
   }
 
-  Future<ConfirmLoginModel> confirmLogin() async{
+  Future<ConfirmLoginModel> confirmLogin() async {
     final prefs = await SharedPrefsRepository.instance;
     final deviceId = prefs.deviceId;
 
     return CustomDio.authInstance.patch('/login/confirmar', data: {
-      'ios_token': Platform.isIOS? deviceId: null,
-      'android_token': Platform.isAndroid? deviceId: null,
+      'ios_token': Platform.isIOS ? deviceId : null,
+      'android_token': Platform.isAndroid ? deviceId : null,
     }).then((res) => ConfirmLoginModel.fromJson(res.data));
   }
 }
