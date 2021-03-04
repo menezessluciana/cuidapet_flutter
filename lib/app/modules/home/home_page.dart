af0df6ba0511b4145af8a42688a1386bc4d2e951
Luciana Menezes <luciana.menezes@superlogica.com>
2021-03-04 18:04:42 -0300

    customizando a appbar

--- lib/app/modules/home/home_page.dart
+++ lib/app/modules/home/home_page.dart
@@ -1,13 +1,12 @@
 import 'package:cuidapet_curso/app/repository/shared_prefs_repository.dart';
-import 'package:cuidapet_curso/app/shared/auth_store.dart';
+import 'package:cuidapet_curso/app/shared/theme_utils.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+import 'package:flutter_screenutil/screenutil.dart';
 import 'home_controller.dart';
 
 class HomePage extends StatefulWidget {
-  final String title;
-  const HomePage({Key key, this.title = "Home"}) : super(key: key);
+  const HomePage({Key key}) : super(key: key);
 
   @override
   _HomePageState createState() => _HomePageState();
@@ -26,11 +25,70 @@ class _HomePageState extends ModularState<HomePage, HomeController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text(widget.title), actions: <Widget>[
-        IconButton(
-            icon: Icon(Icons.location_on),
-            onPressed: () => Modular.link.pushNamed('/enderecos')),
-      ]),
+      appBar: PreferredSize(
+        preferredSize: Size(double.infinity, 100),
+        child: AppBar(
+          backgroundColor: Colors.grey[100],
+          title: Padding(
+            padding: const EdgeInsets.only(bottom: 12.0),
+            child: Text('Cuidapet', style: TextStyle(color: Colors.white)),
+          ),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.location_on),
+              onPressed: () => Modular.link.pushNamed('/enderecos'),
+            )
+          ],
+          elevation: 0,
+          flexibleSpace: Stack(
+            children: <Widget>[
+              Container(
+                height: 110,
+                width: double.infinity,
+                color: ThemeUtils.primaryColor,
+              ),
+              Align(
+                alignment: Alignment.bottomCenter,
+                child: Container(
+                  width: ScreenUtil.screenWidthDp * .9,
+                  child: Material(
+                    elevation: 4,
+                    borderRadius: BorderRadius.circular(30),
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                        fillColor: Colors.white,
+                        filled: true,
+                        suffixIcon: Padding(
+                          padding: const EdgeInsets.only(right: 20.0),
+                          child: Icon(Icons.search, size: 30),
+                        ),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(30),
+                          borderSide: BorderSide(
+                            color: Colors.grey[200],
+                          ),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(30),
+                          borderSide: BorderSide(
+                            color: Colors.grey[200],
+                          ),
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(30),
+                          borderSide: BorderSide(
+                            color: Colors.grey[200],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
       body: Column(
         children: <Widget>[
           // Text(Modular.get<AuthStore>().usuarioLogado.email),
