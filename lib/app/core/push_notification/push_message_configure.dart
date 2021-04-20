2d5ba2e92b30285515ad209a1ea43af234308132
Luciana Menezes <luciana.menezes@superlogica.com>
2021-04-20 19:02:37 -0300

    configurando firebasemessage

--- lib/app/core/push_notification/push_message_configure.dart
+++ lib/app/core/push_notification/push_message_configure.dart
@@ -6,12 +6,21 @@ import 'package:firebase_messaging/firebase_messaging.dart';
 class PushMessageConfigure {
   final FirebaseMessaging _fcm = FirebaseMessaging();
 
-  void configure() async{
-    if(Platform.isIOS){
+  void configure() async {
+    if (Platform.isIOS) {
       await _fcm.requestNotificationPermissions(IosNotificationSettings());
     }
 
+    _fcm.configure(onMessage: (Map<String, dynamic> message) async {
+      print('onmessage $message');
+    }, onLaunch: (Map<String, dynamic> message) async {
+      print('message $message');
+    }, onResume: (Map<String, dynamic> message) async {
+      print('message $message');
+    });
+
     String deviceId = await _fcm.getToken();
+    print('device id $deviceId');
     final prefs = await SharedPrefsRepository.instance;
     prefs.registerDeviceId(deviceId);
   }
