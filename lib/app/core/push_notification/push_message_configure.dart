27925f7b868b7981ea7950fe5ba5204d8aa661f1
Luciana Menezes <luciana.menezes@superlogica.com>
2021-04-23 15:54:06 -0300

    configurando local notification

--- lib/app/core/push_notification/push_message_configure.dart
+++ lib/app/core/push_notification/push_message_configure.dart
@@ -2,17 +2,47 @@ import 'dart:io';
 
 import 'package:cuidapet_curso/app/repository/shared_prefs_repository.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 class PushMessageConfigure {
   final FirebaseMessaging _fcm = FirebaseMessaging();
+  final FlutterLocalNotificationsPlugin _flutterLocalNotificationsPlugin =
+      FlutterLocalNotificationsPlugin();
 
   void configure() async {
+    final initializationSettingsAndroid =
+        AndroidInitializationSettings('ic_launcher');
+    final initializationSettingsIOS = IOSInitializationSettings(
+      onDidReceiveLocalNotification: _onDidReceiveLocalNotification,
+    );
+
+    final initializationSettings = InitializationSettings(
+        initializationSettingsAndroid, initializationSettingsIOS);
+
+    await _flutterLocalNotificationsPlugin.initialize(initializationSettings);
+
+    final androidPlatformChannelSpecifics = AndroidNotificationDetails(
+        'com.example.cuidapet_curso', 'cuidapet_curso', 'Cuidapet',
+        importance: Importance.Max, priority: Priority.High, ticker: 'ticker');
+
+    final iOSPlataformChannelSpecifics = IOSNotificationDetails();
+    final platformChannelSpecifics = NotificationDetails(
+        androidPlatformChannelSpecifics, iOSPlataformChannelSpecifics);
+
     if (Platform.isIOS) {
       await _fcm.requestNotificationPermissions(IosNotificationSettings());
+      _fcm.onIosSettingsRegistered.listen((settings) {
+        print('Configurações do iOS registrada $settings');
+      });
     }
 
     _fcm.configure(onMessage: (Map<String, dynamic> message) async {
       print('onmessage $message');
+      await _flutterLocalNotificationsPlugin.show(
+          99,
+          message['notification']['title'],
+          message['notification']['body'],
+          platformChannelSpecifics);
     }, onLaunch: (Map<String, dynamic> message) async {
       print('message $message');
     }, onResume: (Map<String, dynamic> message) async {
@@ -23,5 +53,10 @@ class PushMessageConfigure {
     print('device id $deviceId');
     final prefs = await SharedPrefsRepository.instance;
     prefs.registerDeviceId(deviceId);
+  }
+
+  Future _onDidReceiveLocalNotification(
+      int id, String title, String body, String payload) {
+    print(title);
   }
 }
